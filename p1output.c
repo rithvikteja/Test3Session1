9505f3e2efbb58232213680cbfdaac6d9e10bbfa
Gayathri K <95470777+rithvikteja@users.noreply.github.com>
2022-04-13 06:42:01 +0000

    did it

--- p1output.c
+++ p1output.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
 void input(int *num1,int *den1,int *num2,int *den2)
 {
-  printf("enter the values of num1,den1,num2 and den2\n");
-scanf("%d%d%d%d",num1,den1,num2,den2);
+  printf("enter the values of num1,den1, num2,den2,\n");
+  scanf("%d%d%d%d",num1,den1,num2,den2);
 }
 void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
 {
- int i,gcd;
+  int i,gcd;
   *num3=(num1*den2)+(num2*den1);
   *den3=den1*den2;
   for(i=1;i<=*num3 && i<=*den3;i++)
     {
       if(*num3%i==0 && *den3%i==0)
-        gcd=i;
-    }
+          gcd=i;
+      }
   *num3=*num3/gcd;
-    *den3=*den3/gcd;
-}
+  *den3=*den3/gcd;
+  }
 void output(int num1,int den1,int num2,int den2,int num3,int den3)
 {
-  printf("the sum of two fraction is %d/%d\n",num3,den3);
-}
+  printf("sum of two fractions is %d/%d\n",num3,den3);
+  }
 int main()
 {
   int num1,num2,den1,den2,num3,den3;
@@ -28,4 +28,4 @@ int main()
   add(num1,den1,num2,den2,&num3,&den3);
   output(num1,den1,num2,den2,num3,den3);
   return 0;
-}
+  }
--- p2output.c
+++ p2output.c
@@ -1,44 +1,41 @@
 #include<stdio.h>
 struct _fraction
 {
-  int num,den;
+int num,den;
 };
 typedef struct _fraction Fraction;
 Fraction input_fraction()
 {
-   Fraction f;
-   printf("Enter the fraction:");
-  scanf("%d%d", &f.num,&f.den);
+  Fraction f;
+  printf("enter a fraction\n");
+  scanf("%d%d",&f.num,&f.den);
   return f;
 }
 
-Fraction Smallest_fraction(Fraction f1,Fraction f2, Fraction f3)
+Fraction Smallest_fraction(Fraction f1,Fraction f2,Fraction f3)
 {
-  
   Fraction smallest;
-  if(f1.num*f2.den< f2.num*f1.den) 
+  if(f1.num*f2.den<f2.num*f1.den)
   {
-    if (f1.num*f3.den<f3.num*f1.den)
+    if(f1.num*f3.den<f3.num*f1.den)
     smallest.num=f1.num;
     smallest.den=f1.den;
   }
-  else if (f2.num*f3.den<f3.num*f2.den)
+  else if(f2.num*f3.den<f3.num*f2.den)
   {
     smallest.num=f2.num;
     smallest.den=f2.den;
   }
-  else 
+else
   {
     smallest.num=f3.num;
     smallest.den=f3.den;
   }
   return smallest;
 }
-
-void output(Fraction f1, Fraction f2, Fraction f3, Fraction smallest)
+void output(Fraction f1,Fraction f2,Fraction f3,Fraction smallest)
 {
-
-  printf("the smallest fraction is %d/%d", smallest.num,smallest.den);
+  printf("smallest fraction is %d/%d\n",smallest.num,smallest.den);
 }
 int main()
 {
