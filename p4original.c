7beed48ce52f99a5f978f9b2316d6fce16d52445
Gayathri K <95470777+rithvikteja@users.noreply.github.com>
2022-04-13 12:20:25 +0000

    i did it

--- p4original.c
+++ p4original.c
@@ -1,12 +1,49 @@
-int ncr(int n,int r)
+#include<stdio.h>
+int input_degree()
 {
-int fact(int n),ncr,sum; 
-  int i;
-  ncr=fact(n)/fact(r)*fact(n-r);  
-for(i=1;i<=n;i++)
-  {
-    sum=sum*i;
-  }
+  int n;
+  printf("enter a degree\n");
+  scanf("%d",&n);
+  return n;
+}
+float input_x()
+{
+  float x;
+  printf("enter the value of x\n");
+  scanf("%f",&x);
+  return x;
+}
+void input_coefficients(int n,float a[n])
+{
+  for(int i=0;i<=n;i++)
+    {
+      printf("enter the coefficients=x^%d\n",i);
+      scanf("%f",&a[i]);
+    }
+}
+float evaluate_polynomial(int n,float a[n],float x)
+{
+  int sum=0;
+  for(int i=n;i>0;i--)
+    {
+      sum=(sum+a[i])*x;
+      }
+  sum=sum+a[0];
   return sum;
-
+  }
+void out_put(int n,float a[n],float x,float result)
+{
+  printf("%f\n",result);
+}
+int main()
+{
+  int n;
+  float x;
+  n=input_degree();
+  x=input_x();
+  float a[n],result;
+  input_coefficients(n,a);
+  result=evaluate_polynomial(n,a,x);
+  out_put(n,a,x,result);
+  return 0;
   }
